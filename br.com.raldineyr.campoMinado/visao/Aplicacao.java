16639652fe234820f816aad7110070c314e1e0b2
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-09-29 22:02:40 -0300

    Create Aplicacao.java

---
+++ br.com.raldineyr.campoMinado/visao/Aplicacao.java
@@ -0,0 +1,16 @@
+package br.com.raldineyr.campoMinado;
+
+import br.com.raldineyr.campoMinado.modelo.Tabuleiro;
+import br.com.raldineyr.campoMinado.visao.TabuleiroConsole;
+
+public class Aplicacao {
+	
+	public static void main(String[] args) {
+		
+		// coluna e linha
+		Tabuleiro tabuleiro = new Tabuleiro(6,6,6);
+		
+		new TabuleiroConsole(tabuleiro);
+
+	}
+}
