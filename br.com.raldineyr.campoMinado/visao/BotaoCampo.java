1cbfa8b363b5d0bfe86c46870ed0e56ddd5748a4
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-10-11 09:32:39 -0300

    Update BotaoCampo.java

--- br.com.raldineyr.campoMinado/visao/BotaoCampo.java
+++ br.com.raldineyr.campoMinado/visao/BotaoCampo.java
@@ -2,34 +2,38 @@ package br.com.raldineyr.campoMinado.visao;
 
 import java.awt.Color;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.SwingUtilities;
+
 import br.com.raldineyr.campoMinado.modelo.Campo;
 import br.com.raldineyr.campoMinado.modelo.CampoEvento;
 import br.com.raldineyr.campoMinado.modelo.CampoObservador;
 
 @SuppressWarnings("serial")
-public class BotaoCampo extends JButton implements CampoObservador {
+public class BotaoCampo extends JButton implements CampoObservador, MouseListener{
 	
-
 	private final Color BG_PADRAO = new Color(184, 184, 184);
 	private final Color BG_MARCAR = new Color(8, 179, 247);
 	private final Color BG_EXPLODIR = new Color(189, 66, 68);
 	private final Color TEXTO_VERDE = new Color(0, 100, 0);
-
 	
 	private Campo campo;
 	
-	public BotaoCampo (Campo campo) {
+	public BotaoCampo (Campo campo){
 		
 		this.campo = campo;
-		// setBackground(BG_PADRAO);
+		setBackground(BG_PADRAO);
+		setOpaque(true);
 		setBorder(BorderFactory.createBevelBorder(0));
 		
+		addMouseListener(this);
 		campo.registarObservador(this);
-
-	}
-
+	} 
+	
 	@Override
 	public void eventoOcorreu(Campo campo, CampoEvento evento) {
 		
@@ -50,26 +54,94 @@ public class BotaoCampo extends JButton implements CampoObservador {
 			default:
 			aplicarEstiloPadrao();
 		}
+		
+		SwingUtilities.invokeLater(() -> {
+			repaint();
+			validate();
+		});
 	
 	}
 
 	private void aplicarEstiloPadrao() {
-	
 		
+		setBackground(BG_PADRAO);
+		setBorder(BorderFactory.createBevelBorder(0));
+		setText("");
 	}
 
 	private void aplicarEstiloExplodir() {
 		
+		setBackground(BG_EXPLODIR);
+		setForeground(Color.WHITE);
+		setText("X");
 		
 	}
 
 	private void aplicarEstiloMarcar() {
-
+		
+		setBackground(BG_MARCAR);
+		setForeground(Color.BLACK);
+		setText("M");
 		
 	}
 
 	private void aplicarEstiloAbrir() {
 		
-	}		
+		if(campo.isMinado()) {
+			
+			setBackground(BG_EXPLODIR);	
+			return;	
+		}
+		
+		setBackground(BG_PADRAO);
+		setBorder(BorderFactory.createLineBorder(Color.GRAY));
+	
+		switch(campo.minasVizinhanca()) {
+		
+		case 1: 
+			setForeground(TEXTO_VERDE);
+			break;
+			
+		case 2:
+			setForeground(Color.BLUE);
+			break;
+			
+		case 3:
+			setForeground(Color.YELLOW);
+			break;
+			
+		case 4:
+		case 5:
+		case 6:
+			setForeground(Color.RED);
+			break;
+			
+		default:
+			setForeground(Color.PINK);
+		}
+		
+		String valor = !campo.vizinhancaSegura() ? 
+				campo.minasVizinhanca() + "" : "";
+		setText(valor);
+	}	
+	
+	// Interace dos eventos do mouse
+	
+	@Override
+	public void mousePressed (MouseEvent e) {
+		
+		if(e.getButton() == 1) {
+			campo.abrir();
+			
+		} else {
+			campo.alternarMarcacao();
+		}
+	}
+	
+	public void mouseClicked(MouseEvent e) {}
+	public void mouseEntered(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {}
+	public void mouseRelesead(MouseEvent e) {}
+	public void mouseReleased(MouseEvent e) {}
 
 }
